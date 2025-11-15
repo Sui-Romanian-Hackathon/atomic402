d96d250ee19d043f2a274ed00fa75226d88f30b1
kevo <akerimberdi@gmail.com>
2025-11-15 10:10:17 +0200

    shadcn init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,36 +1,10 @@
-import Image, { type ImageProps } from "next/image";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
+import { Button } from "~/components/ui/button";
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1>Hello World</h1>
+    <div>
+      <main>
+        <h1 className="text-3xl font-bold underline">Hello World</h1>
+        <Button>Click me</Button>
       </main>
     </div>
   );
