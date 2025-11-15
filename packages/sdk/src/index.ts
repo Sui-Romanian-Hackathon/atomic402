ad1e39efdccc756dca6038c42c18b5d33833c9e4
kevo <akerimberdi@gmail.com>
2025-11-15 14:48:30 +0200

    export types

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,12 @@
 // SDK exports
-export * from "./client";
+export * from "./client.js";
+export * from "./server.js";
 
+// Re-export types from shared
+export type {
+  X402Response,
+  ContentMetadata,
+  AccessReceiptData,
+  SignedTransactionRequest,
+  TransactionResult,
+} from "@repo/shared/types";
